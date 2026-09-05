47ff9f4cbb3d9d5ad753e90324e08b8396f05e33
shinjinee15 <shinjineebera@gmail.com>
2026-09-05 07:07:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,43 @@
-print('hello from my project')
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(page_title = "Gapminder Dashboard", layout='wide')
+
+@st.cache_data
+def load_data():
+   return px.data.gapminder()
+
+df = load_data()
+
+st.title("County Development Dashboard")
+
+# ---- Filter panel(sidebar) ----
+st.sidebar.header("Filters")
+years = sorted(df["year"].unique())
+selected_year = st.sidebar.select_slider("Year", options=years , value=years[-1])
+continents = sorted(df["continent"].unique())
+selected_continents = st.sidebar.multiselect("Continent", continents, default=continents)
+filtered = df[(df["year"] == selected_year) & (df["continent"].isin(selected_continents))]
+min_population = st.sidebar.slider("Minimum Population",min_value=0,max_value=200_000_000,value=0,step=5_000_000)
+filtered = df[(df["year"] == selected_year) &(df["continent"].isin(selected_continents)) &(df["pop"] >= min_population)]
+
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Countries shown", f"{filtered['country'].nunique()}")
+col2.metric("Median life expectancy", f"{filtered['lifeExp'].median():.1f} yrs")
+col3.metric("Total population", f"{filtered['pop'].sum()/1e9:.2f} B")
+
+# ---- Main chart ----
+fig=px.scatter(filtered,x="gdpPercap", y="lifeExp",size="pop",color="continent",hover_name="country", log_x=True, size_max=60,
+    title = f"GDP per Capita vs Life Expectancy in {selected_year}"
+)
+st.plotly_chart(fig, use_container_width = True)
+# ---- Drill-down ----
+st.subheader("Drill down into a country")
+country_pick = st.selectbox("Choose a country", sorted(filtered["country"].unique()))
+country_hist = df[df["country"] == country_pick]
+fig2 = px.line(country_hist, x="year", y="lifeExp", markers=True,
+               title=f"Life expectancy over time - {country_pick}")
+st.plotly_chart(fig2, use_container_width=True)
+
